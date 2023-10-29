baaf60faf4ea90fc5bc8d9c2fb0b1d82b91e3bad
codesage <laughingsagecoder@gmail.com>
2023-10-29 11:34:35 +0200

    adding feature section on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,13 @@ export default function Home() {
       {/* value proposition section */}
       <div>
         <div className="relative isolate">
-          <div
-            aria-hidden='true'
-            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+          {/* Decorative */}
+          <div aria-hidden='true' className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
             <div
               style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}
               className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
             />
           </div>
-
           <div>
             <div className='mx-auto max-w-6xl px-6 lg:px-8'>
               <div className='mt-16 flow-root sm:mt-24'>
@@ -52,10 +50,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-
-          <div
-            aria-hidden='true'
-            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+          {/* Decorative */}
+          <div aria-hidden='true' className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
             <div
               style={{
                 clipPath:
@@ -63,6 +59,21 @@ export default function Home() {
               }}
               className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
             />
+          </div>
+        </div>
+      </div>
+
+      {/* Feature section */}
+      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+              Start chatting in minutes
+            </h2>
+            <p className='mt-4 text-lg text-gray-600'>
+              Chatting to your PDF files has never been
+              easier than with Docu-AI.
+            </p>
           </div>
         </div>
       </div>
