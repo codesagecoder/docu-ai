99b25ce734d4b05194885de4dcb148fce03a4cd7
codesage <laughingsagecoder@gmail.com>
2023-10-29 11:42:39 +0200

    adding steps to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       {/* Feature section */}
       <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
         <div className='mb-12 px-6 lg:px-8'>
@@ -76,6 +75,57 @@ export default function Home() {
             </p>
           </div>
         </div>
+        {/* steps */}
+        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Step 1
+              </span>
+              <span className='text-xl font-semibold'>
+                Sign up for an account
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Either starting out with a free plan or
+                choose our{' '}
+                <Link
+                  href='/pricing'
+                  className='text-blue-700 underline underline-offset-2'>
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Step 2
+              </span>
+              <span className='text-xl font-semibold'>
+                Upload your PDF file
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                We&apos;ll process your file and make it
+                ready for you to chat with.
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Step 3
+              </span>
+              <span className='text-xl font-semibold'>
+                Start asking questions
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                It&apos;s that simple. Try out Docu-AI today -
+                it really takes less than a minute.
+              </span>
+            </div>
+          </li>
+        </ol>
       </div>
     </>
   )
