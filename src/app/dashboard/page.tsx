2999108afe104560fc44c774385231c1d1f1b887
codesage <laughingsagecoder@gmail.com>
2023-11-03 11:51:42 +0200

    building dashboard skeleton

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from 'next/navigation'
+
+const Page = () => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
+
+    user.email;
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Page;
