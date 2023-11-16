715c654da540b931a73591de2084af55da04d5a8
codesage <laughingsagecoder@gmail.com>
2023-11-16 23:33:01 +0200

    building the delete file procedure

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
+import { z } from 'zod';
 
 export const appRouter = router({
     authCallback: publicProcedure.query(async () => {
@@ -30,6 +31,21 @@ export const appRouter = router({
         return await db.file.findMany({
             where: { id: userId }
         });
+    }),
+    deleteFile: privateProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
+        const { userId } = ctx;
+
+        const file = await db.file.findFirst({
+            where: { id: input.id, userId }
+        });
+
+        if (!file) throw new TRPCError({ code: 'NOT_FOUND' });
+
+        await db.file.delete({
+            where: { id: input.id }
+        });
+
+        return file;
     })
 });
 
