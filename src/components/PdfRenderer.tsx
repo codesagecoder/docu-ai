d4c7ced73aeee91269e2584385ded605424088ed
codesage <laughingsagecoder@gmail.com>
2023-11-28 22:08:21 +0200

    rendering pdfs

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,16 @@
-import { Document } from 'react-pdf';
+'use client'
 
-const PdfRenderer = ({ url }: { url: string }) => {
+import { Document,Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+interface PdfRendererProps {
+    url: string;
+};
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
     return (
         <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
             <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
@@ -10,7 +20,9 @@ const PdfRenderer = ({ url }: { url: string }) => {
             </div>
             <div className="flex-1 w-full max-h-screen">
                 <div>
-                    <Document />
+                    <Document file={url} className='max-h-full' >
+                        <Page pageNumber={1}/>
+                    </Document>
                 </div>
             </div>
         </div>
