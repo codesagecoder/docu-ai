8c5005852b9d8e54e30124c3c22345b8dea0d1af
codesage <laughingsagecoder@gmail.com>
2023-11-29 19:27:37 +0200

    implementing pdf reader error handler

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -4,6 +4,7 @@ import { Loader2 } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
+import { useToast } from './ui/use-toast';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -12,6 +13,7 @@ interface PdfRendererProps {
 };
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+    const { toast } = useToast();
     return (
         <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
             <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
@@ -24,9 +26,12 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                     <Document
                         loading={
                             <div className='flex justify-center'>
-                                <Loader2 className='my-24 h-6 w-6 animate-spin'/>
+                                <Loader2 className='my-24 h-6 w-6 animate-spin' />
                             </div>
                         }
+                        onLoadError={() => {
+                            toast({ title: 'Error loading PDF', description: 'Please try again later', variant: 'destructive' });
+                        }}
                         file={url}
                         className='max-h-full'
                     >
