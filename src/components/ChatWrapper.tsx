440b11e2d6ce8d8490efbe7666e82481d7ce2172
codesage <laughingsagecoder@gmail.com>
2023-12-06 22:28:47 +0200

    building chat wrapper skeleton

--- src/components/ChatWrapper.tsx
+++
@@ -1,5 +0,0 @@
-const ChatWrapper = () => {
-    return (<div></div>);
-};
-
-export default ChatWrapper;
---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,7 @@
+const ChatInput = () => {
+    return (
+    <div className=""></div>
+    );
+};
+
+export default ChatInput;
