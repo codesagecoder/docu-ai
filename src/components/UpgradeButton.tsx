563447981a27d4e892ee0f530b7c896e4c75a36b
codesage <laughingsagecoder@gmail.com>
2023-12-22 22:18:30 +0200

    refactoring upgrade button component

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -5,11 +5,11 @@ import { Button } from './ui/button'
 
 const UpgradeButton = () => {
 
-  return (
-    <Button className='w-full'>
-      Upgrade now <ArrowRight className='h-5 w-5 ml-1.5' />
-    </Button>
-  )
+    return (
+        <Button className='w-full'>
+            Upgrade now <ArrowRight className='h-5 w-5 ml-1.5' />
+        </Button>
+    )
 }
 
 export default UpgradeButton
