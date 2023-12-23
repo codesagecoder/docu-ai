01b416f31694615858626ab9378fb5844a717363
codesage <laughingsagecoder@gmail.com>
2023-12-23 21:27:02 +0200

    integrtating stripe session into upgrade button

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -2,11 +2,18 @@
 
 import { ArrowRight } from 'lucide-react'
 import { Button } from './ui/button'
+import { trpc } from '@/app/_trpc/client'
 
 const UpgradeButton = () => {
 
+    const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+        onSuccess: ({ url }) => {
+            window.location.href = url ?? '/dashboard/billing';
+        }
+    });
+
     return (
-        <Button className='w-full'>
+        <Button className='w-full' onClick={() => createStripeSession()}>
             Upgrade now <ArrowRight className='h-5 w-5 ml-1.5' />
         </Button>
     )
