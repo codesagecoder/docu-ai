ca04b10b3e6f7595b415f7a6c259636611e9f62a
codesage <laughingsagecoder@gmail.com>
2023-12-28 19:29:29 +0200

    changing hard coded url path in trpc provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { trpc } from '@/app/_trpc/client'
+import { absoluteUrl } from '@/lib/utils';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { PropsWithChildren, useState } from 'react';
 
 const Providers = ({ children }: PropsWithChildren) => {
     const [queryClient] = useState(() => new QueryClient());
-    const [trpcClient] = useState(() => trpc.createClient({ links: [httpBatchLink({ url: "/api/trpc" })] }));
+    const [trpcClient] = useState(() => trpc.createClient({ links: [httpBatchLink({ url: absoluteUrl("/api/trpc") })] }));
 
     return (
         <trpc.Provider
